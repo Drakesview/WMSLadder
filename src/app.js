b601d330066ecebaaea58c906b8c2ad05d7d07aa
Adam Shenton <shenton_a@wmsldom.wmsl.co.uk>
2019-10-22 17:02:49 +0100

    New cloud server

--- src/app.js
+++ src/app.js
@@ -9,43 +9,83 @@ import './styles/styles.scss'
 import 'react-dates/lib/css/_datepicker.css';
 import 'react-dates/initialize';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {firebase} from './firebase/firebase'
+import {firebase}  from './firebase/firebase'
 import LoadingPage from './components/LoadingPage'
 import {startGetLadder} from './actions/ladder'
+import database  from './firebase/firebase'
+
+// database.collection("users").add({
+//     first: "Ada",
+//     last: "Lovelace",
+//     middle: "shents",
+//     born: 1815
+//   })
+//   .then(function(docRef) {
+//     console.log("Document written with ID: ", docRef.id);
+//   })
+//   .catch(function(error) {
+//     console.log("Error adding document: ", error);
+//   });
+
+database.collection("users").orderBy('pos').get().then((querySnapshot) => {
+    const users = []
+    querySnapshot.forEach((doc) => {
+        users.push({
+            id:doc.id,
+            ...doc.data()
+        })
+        console.log(doc.id, '=>', doc.data())
+    })
+    console.log(users)
+})
 
 
-const store = configureStore();
-
-const jsx = (
-    <Provider store={store}>
-        <AppRouter />
-    </Provider>
-);
-
-let hasRendered = false;
-const renderApp = () => {
-    if (!hasRendered) {
-        ReactDOM.render(jsx, document.getElementById('app'))
-        hasRendered = true;
-    }
-}
+database.collection("users").where('middle', '==', 'shents').get()
+    .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            console.log(doc.id, "=>", doc.data())
+        })
+    })
+    .catch((e) => {
+        console.log(`Error getting documents: ${e}`)
+    });
 
 ReactDOM.render(<LoadingPage />, document.getElementById('app'))
 
+// const store = configureStore();
 
-firebase.auth().onAuthStateChanged((user) => {
-    if(user) {
-        store.dispatch(login(user.uid))
-        store.dispatch(startGetLadder()).then(() => {
-            renderApp();
-            if(history.location.pathname === '/') {
-               history.push('/dashboard'); 
-            }
-        });
-    } else {
-        store.dispatch(logout())
-        renderApp();
-        history.push('/');
-    }
-});
+
+// const jsx = (
+//     <Provider store={store}>
+//         <AppRouter />
+//     </Provider>
+// );
+
+// let hasRendered = false;
+// const renderApp = () => {
+//     if (!hasRendered) {
+//         ReactDOM.render(jsx, document.getElementById('app'))
+//         hasRendered = true;
+//     }
+// }
+
+// ReactDOM.render(<LoadingPage />, document.getElementById('app'))
+
+
+// firebase.auth().onAuthStateChanged((user) => {
+//     if(user) {
+//         store.dispatch(login(user.uid))
+//         renderApp();
+//             if(history.location.pathname === '/') {
+//                history.push('/dashboard'); 
+//             }
+//         // store.dispatch(startGetLadder()).then(() => {
+            
+//         // });
+//     } else {
+//         store.dispatch(logout())
+//         renderApp();
+//         history.push('/');
+//     }
+// });
 
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,32 +1,20 @@
-import * as firebase from 'firebase'
+import * as firebase from 'firebase/app'
+import 'firebase/firestore'
 
 const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    // databaseURL: process.env.FIREBASE_DATABASE_URL,
     projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID
+    // storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    // messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    // appId: process.env.FIREBASE_APP_ID
   };
   
 firebase.initializeApp(firebaseConfig);
 
-const database = firebase.database()
-
-
-const testUser = database.ref("ladder/users")
-
-testUser.equalTo("TSLYusr8xmd8gUUfDNO0260qqT43").once("value").then((snapshot) => {
-  const users = []
-  snapshot.forEach((childSnapshot) => {
-    users.push({
-      id:childSnapshot.key,
-      ...childSnapshot.val()
-    })
-  })
-  console.log(users)
-});
+// const database = firebase.database()
+const database = firebase.firestore();
 
 
 export {firebase, database as default}

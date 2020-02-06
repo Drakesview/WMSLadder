f6e117cc2c36586a116f1fdbff6c8d68eba04fd7
Adam Shenton <shenton_a@wmsldom.wmsl.co.uk>
2020-02-06 14:39:53 +0000

    setting up env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -14,7 +14,7 @@ const firebaseConfig = {
   };
   
 firebase.initializeApp(firebaseConfig);
-console.log(firebaseConfig.projectId)
+
 // const database = firebase.database()
 const database = firebase.firestore();
 
